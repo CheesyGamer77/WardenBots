e9bdd5f096be6aacba91cfae13a067981d3653f7
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-16 13:09:34 -0400

    Make Hasher util class final

--- src/main/java/pw/cheesygamer77/wardenbots/internal/Hasher.java
+++ src/main/java/pw/cheesygamer77/wardenbots/internal/Hasher.java
@@ -3,7 +3,7 @@ package pw.cheesygamer77.wardenbots.internal;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class Hasher {
+public final class Hasher {
     public static @NotNull String hashify(@NotNull String value) {
         return new DigestUtils("SHA3-256").digestAsHex(value);
     }
