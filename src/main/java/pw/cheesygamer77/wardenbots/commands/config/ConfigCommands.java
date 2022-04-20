fc4adbd9227207e5f55cb09bec54aaa7d94fb401
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-20 13:02:55 -0400

    Lock `/config` commands behind `MANAGE_SERVER` permissions

--- src/main/java/pw/cheesygamer77/wardenbots/commands/config/ConfigCommands.java
+++ src/main/java/pw/cheesygamer77/wardenbots/commands/config/ConfigCommands.java
@@ -4,12 +4,11 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.jetbrains.annotations.NotNull;
-import pw.cheesygamer77.cheedautilities.commands.slash.SlashCommand;
+import pw.cheesygamer77.wardenbots.internal.commands.PermissionLockedSlashCommand;
 
-public class ConfigCommands extends SlashCommand {
+public class ConfigCommands extends PermissionLockedSlashCommand {
     public ConfigCommands() {
-        super(Commands.slash("config", "Guild bot configuration commands"));
-        addPredicate(event -> event.getMember() != null && event.getMember().hasPermission(Permission.MANAGE_SERVER));
+        super(Commands.slash("config", "Guild bot configuration commands"), Permission.MANAGE_SERVER);
         addSubcommand(new ModLogSubcommand());
     }
 
