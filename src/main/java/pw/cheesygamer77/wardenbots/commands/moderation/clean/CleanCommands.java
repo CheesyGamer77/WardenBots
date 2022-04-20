fcfb2b8fcd821ac213a69474bee9e82f4800c133
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-20 13:03:55 -0400

    Lock `/clean` commands behind `MESSAGE_MANAGE` permissions

--- src/main/java/pw/cheesygamer77/wardenbots/commands/moderation/clean/CleanCommands.java
+++ src/main/java/pw/cheesygamer77/wardenbots/commands/moderation/clean/CleanCommands.java
@@ -7,11 +7,9 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
-import pw.cheesygamer77.cheedautilities.commands.slash.SlashCommand;
+import pw.cheesygamer77.wardenbots.internal.commands.PermissionLockedSlashCommand;
 
-import java.util.Objects;
-
-public class CleanCommands extends SlashCommand {
+public class CleanCommands extends PermissionLockedSlashCommand {
     public static final OptionData COUNT_OPTION = new OptionData(
             OptionType.INTEGER, "count", "The number of messages to clean")
             .setRequired(true)
@@ -22,8 +20,7 @@ public class CleanCommands extends SlashCommand {
             .setChannelTypes(ChannelType.TEXT);
 
     public CleanCommands() {
-        super(Commands.slash("clean", "Commands to clean up unwanted messages"));
-        addPredicate(event -> event.getGuild() != null && Objects.requireNonNull(event.getMember()).hasPermission(Permission.MESSAGE_MANAGE));
+        super(Commands.slash("clean", "Commands to clean up unwanted messages"), Permission.MESSAGE_MANAGE);
         addSubcommand(new AnySubcommand());
         addSubcommand(new BotsSubcommand());
     }
